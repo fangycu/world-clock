141730d9d7a4806a4f31c426ed266b531b5c6eb3
fangycu <wannapen.s@gmail.com>
2025-01-20 01:06:08 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -45,3 +45,24 @@ laTime.innerHTML = `${moment()
 
 
     })
+
+
+    function updateCity(event){
+        let cityTimeZone = event.target.value;
+        let cityTime=moment().tz(cityTimeZone);
+        let citiesElement=document.querySelector("#cities");
+        citiesElement.innerHTML = `
+         <div class="city">
+      <div>
+        <h2> ${cityTimeZone} </h2>
+        <div class="date"> ${cityTime.format("MMMM Do YYYY")} </div>
+      </div>
+        <div class="time"> ${cityTime.format("h:mm:ss")}  <small> ${cityTime.format("A")}</small></div>
+    </div>
+        `;
+    }
+    updateTime();
+    setInterval(updateTime,1000);
+
+    let citiesSelectElement = document.querySelector("#contries");
+    citiesSelectElement.addEventListener("change",updateCity);
